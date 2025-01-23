d0bfa5fc8c20b75bc3f52d215500ec1d070b3ce6
gitboosting <chanfelix@gmail.com>
2025-01-23 09:46:51 -0600

    able to read acc and gyr consistently

--- R/imufserial.R
+++ R/imufserial.R
@@ -2,21 +2,34 @@ getData <- function(port) {
   #
   # receive data from serial port and print it
   con <- serial::serialConnection(name = "testcon", port = port,
-                          mode = "115200,n,8,1" ,newline = 1 ,translation = "crlf"
+                          mode = "115200,n,8,1", newline = 1, translation = "crlf"
   )
 
   # let's open the serial interface
 
   open(con)
 
-  Sys.sleep(1)
+  # Sys.sleep(1)
 
   # write some stuff
   # serial::write.serialConnection(con,"Hello World!")
 
   # read, in case something came in
-  a <- serial::read.serialConnection(con)
-  print(a)
+  n = 0
+  while (TRUE) {
+    nInQ <- serial::nBytesInQueue(con)["n_in"]
+    if(nInQ > 32) {
+      a <- serial::read.serialConnection(con)
+      print(a)
+      n <- n + 1
+      if (n > 10) {
+        break
+      }
+    }
+  }
+
+  # a <- serial::nBytesInQueue(con)["n_in"]
+  # print(a)
 
   # show summary
   summary(con)
---
+++ srcino/bmi270/bmi270.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include "SparkFun_BMI270_Arduino_Library.h"
+
+// Create a new sensor object
+BMI270 imu;
+
+// I2C address selection
+uint8_t i2cAddress = BMI2_I2C_PRIM_ADDR; // 0x68
+//uint8_t i2cAddress = BMI2_I2C_SEC_ADDR; // 0x69
+
+void setup()
+{
+    // Start serial
+    Serial.begin(115200);
+    // Serial.println("BMI270 Example 1 - Basic Readings I2C");
+
+    // Initialize the I2C library
+    Wire.begin();
+
+    // Check if sensor is connected and initialize
+    // Address is optional (defaults to 0x68)
+    while(imu.beginI2C(i2cAddress) != BMI2_OK)
+    {
+        // Not connected, inform user
+        Serial.println("Error: BMI270 not connected, check wiring and I2C address!");
+
+        // Wait a bit to see if connection is established
+        delay(1000);
+    }
+
+    // Serial.println("BMI270 connected!");
+}
+
+void loop()
+{
+    // Get measurements from the sensor. This must be called before accessing
+    // the sensor data, otherwise it will never update
+    imu.getSensorData();
+
+    // output decimal places
+    uint8_t prec = 6;
+
+    // Print acceleration data
+    Serial.print(imu.data.accelX, prec);
+    Serial.print(",");
+    Serial.print(imu.data.accelY, prec);
+    Serial.print(",");
+    Serial.print(imu.data.accelZ, prec);
+    Serial.print(",");
+
+    // Print rotation data
+    Serial.print(imu.data.gyroX, prec);
+    Serial.print(",");
+    Serial.print(imu.data.gyroY, prec);
+    Serial.print(",");
+    Serial.println(imu.data.gyroZ, prec);
+
+    // Print 50x per second
+    delay(20);
+}
