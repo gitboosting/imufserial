01254f8214ef13116d8b66a011b587aaa16f730b
gitboosting <chanfelix@gmail.com>
2025-02-05 10:06:57 -0600

    add logic to make processing more robust

--- R/imufserial.R
+++ R/imufserial.R
@@ -1,3 +1,19 @@
+library(tidyverse)
+
+getMillis <- function() {
+  # function to get current time in milli seconds
+  as.numeric(Sys.time())*1000
+}
+
+getMillisDiff <- function() {
+  old <- getMillis()
+  for (i in 1:1e8) {
+    # do nothing
+  }
+  new <- getMillis()
+  new - old
+}
+
 getData <- function(port) {
   #
   # receive data from serial port and print it
@@ -6,7 +22,8 @@ getData <- function(port) {
   )
 
   # let's open the serial interface
-
+  close(con)
+  Sys.sleep(2)
   open(con)
 
   # Sys.sleep(1)
@@ -16,6 +33,7 @@ getData <- function(port) {
 
   # read, in case something came in
   n = 0
+  oldMillis <- getMillis()
   while (TRUE) {
     inQ <- serial::nBytesInQueue(con)
     nInQ <- inQ["n_in"]
@@ -23,9 +41,14 @@ getData <- function(port) {
     if(nInQ > 32) {
       # print(inQ)
       a <- serial::read.serialConnection(con)
+      a <- stringr::str_split_1(a, ",") %>% trimws() %>% as.numeric() %>% suppressWarnings()
+      if (length(a) != 6) next
+      newMillis <- getMillis()
+      print(newMillis - oldMillis)
       print(a)
       n <- n + 1
-      if (n > 100) {
+      oldMillis <- newMillis
+      if (n > 500) {
         break
       }
     }
